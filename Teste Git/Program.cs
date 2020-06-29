cb0363f0ea4bcc5321891576eea32cbf56966151
Midney Amorim <amorim@mticampinas.com.br>
2020-06-29 09:00:35 -0300

    Acrescentado Bom dia

--- Teste Git/Program.cs
+++ Teste Git/Program.cs
@@ -4,6 +4,7 @@ namespace Teste_Git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
 
             Console.ReadLine();
         }
