ce3a160c534bdaf98a6c0319086142a25dde0ee1
Midney Amorim <amorim@mticampinas.com.br>
2020-06-29 09:01:29 -0300

    Acrescentado como voce esta

--- Teste Git/Program.cs
+++ Teste Git/Program.cs
@@ -5,6 +5,7 @@ namespace Teste_Git {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está");
 
             Console.ReadLine();
         }
